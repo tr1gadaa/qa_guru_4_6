700207e88ddbc5d6ce83be920f3607e5a91f44e8
tr1gadaa <118889981+tr1gadaa@users.noreply.github.com>
2023-04-01 15:02:35 +0300

    add make_readble_function

--- homework.py
+++ homework.py
@@ -59,6 +59,8 @@ def test_find_suitable_user():
 # >>> open_browser(browser_name="Chrome")
 # "Open Browser [Chrome]"
 
+def make_readble_function(func_name, *args):
+    return f"{func_name.__name__.replace('_', ' ').title()} [{', '.join(args)}]"
 
 def test_readable_function():
     open_browser(browser_name="Chrome")
@@ -66,13 +68,13 @@ def test_readable_function():
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = make_readble_function(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = make_readble_function(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = make_readble_function(find_registration_button_on_login_page, page_url,button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
